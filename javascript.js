4bc66b3ec180608dd73ee2539fe2381148d2fdd2
das-ale <141550503+das-ale@users.noreply.github.com>
2023-08-22 15:13:39 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,20 @@
-const max = 57;
-let actual = max - 13;
-let percentage = actual / max;
+
+function add7(num1) {
+    return parseInt(num1) + 7;
+}
+
+function multiply(num1, num2) {
+    return parseInt(num1)*parseInt(num2);
+}
+
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1); 
+}
+
+function lastLetter(word) {
+    return word.charAt(word.length - 1);
+}
+
+let add9 = (num1) => (num1) * 9;
+
+alert(add9(3));
