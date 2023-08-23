d4b35b2a622d7d43cdfbe672621a5be78fb0c472
das-ale <141550503+das-ale@users.noreply.github.com>
2023-08-23 10:55:39 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -15,6 +15,15 @@ function lastLetter(word) {
     return word.charAt(word.length - 1);
 }
 
-let add9 = (num1) => (num1) * 9;
+// let add9 = (num1) => (num1) * 9;
 
-alert(add9(3));
+// alert(add9(3));
+
+
+let listaNumeros=[1,3,2,20,34,12,43];
+
+let numerosOrdenados=listaNumeros.sort((a,b) => a-b);
+
+
+alert(numerosOrdenados);
+alert(numerosOrdenados[(numerosOrdenados.length)-3]);
